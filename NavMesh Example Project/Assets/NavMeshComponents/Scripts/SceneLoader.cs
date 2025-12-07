47a182d17116395b51b5352f5731504e0bdcad93
Mschoen03 <mschoen03a@gmail.com>
2025-12-07 11:11:25 -0600

    fin

---
+++ NavMesh Example Project/Assets/NavMeshComponents/Scripts/SceneLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadFinished01()
+    {
+        SceneManager.LoadScene("Finished01");
+    }
+
+    public void LoadFinished02()
+    {
+        SceneManager.LoadScene("Finished02");
+    }
+
+    public void LoadFinished03()
+    {
+        SceneManager.LoadScene("Finished03");
+    }
+
+    public void LoadFinished04()
+    {
+        SceneManager.LoadScene("Finished04");
+    }
+
+    public void LoadFinished05()
+    {
+        SceneManager.LoadScene("Finished05");
+    }
+
+    public void LoadFinished06()
+    {
+        SceneManager.LoadScene("Finished06");
+    }
+}
